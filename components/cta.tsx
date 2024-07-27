86747cd4b145bc5ea3343e5ee567666a93843f5e
cy-techAI <cytechai1@gmail.com>
2024-07-27 20:29:01 +0530

    Update cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -5,7 +5,6 @@ export default function Cta() {
         <div className="pt-12 md:pt-20 border-t border-b border-gray-800">
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16" data-aos-id-cta>
 
-            {/* Section header /}
             <h1 className="h2 mb-4" data-aos="fade-up" data-aos-anchor="[data-aos-id-cta]">Ready to secure your digital infrastrucutre?</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200" data-aos-anchor="[data-aos-id-cta]">With Cy-tech AI you can focus on developing better products and leave your digital security to us.</p>
 
